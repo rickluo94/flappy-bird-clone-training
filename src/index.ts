a4c946bc09bc895c3b2bec6f0aa3297b4221581b
rick <edcrfvlily5@gmail.com>
2026-04-01 14:56:38 +0800

    [modify] typescript version

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import Phaser from "phaser";
+
+const gameWidth = 800;
+const gameHeight = 600;
+const flapVelocity = 250;
+const initialBirdPosition = { x: gameWidth * 0.1, y: gameHeight / 2 };
+const pipeX = 400;
+const pipeGap = 100;
+
+let bird: Phaser.Physics.Arcade.Sprite;
+let upperPipe: Phaser.Physics.Arcade.Sprite;
+let lowerPipe: Phaser.Physics.Arcade.Sprite;
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: gameWidth,
+  height: gameHeight,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
+
+function preload(this: Phaser.Scene) {
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
+  this.load.image("pipe", "assets/pipe.png");
+}
+
+function create(this: Phaser.Scene): void {
+  this.add.image(0, 0, "sky").setOrigin(0);
+
+  bird = this.physics.add
+    .sprite(initialBirdPosition.x, initialBirdPosition.y, "bird")
+    .setOrigin(0);
+  bird.setGravityY(400);
+
+  upperPipe = this.physics.add.sprite(pipeX, 100, "pipe").setOrigin(0, 1);
+  lowerPipe = this.physics.add
+    .sprite(pipeX, upperPipe.y + pipeGap, "pipe")
+    .setOrigin(0, 0);
+
+  this.input.on("pointerdown", flap);
+  this.input.keyboard?.on("keydown_SPACE", flap);
+}
+
+function update(): void {
+  if (bird.y > gameHeight || bird.y < -bird.height) {
+    restartBirdPosition();
+  }
+}
+
+function restartBirdPosition(): void {
+  bird.setPosition(initialBirdPosition.x, initialBirdPosition.y);
+  bird.setVelocityY(0);
+}
+
+function flap(): void {
+  bird.setVelocityY(-flapVelocity);
+}
+
+new Phaser.Game(config);
